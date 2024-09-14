6d215ce788ba8d8ed20d9ce60881787a86a9c184
aleksandr Nevolin <yarinsys@gmail.com>
2024-09-14 19:58:25 +0500

    вынес вопрос о повторе в отдельный метод

--- Geni_Idiot_ConsoleApp/Main.cs
+++ Geni_Idiot_ConsoleApp/Main.cs
@@ -1,30 +1,27 @@
 ﻿
+static string[] GetQuestions(int questionsCount)
+{
+    string[] questions = new string[questionsCount];
+    questions[0] = "Сколько будет два плюс два умноженное на два?";
+    questions[1] = "Бревно нужно распилить на 10 частей. Сколько распилов нужно сделать?";
+    questions[2] = "На двух руках 10 пальцев. Сколько пальцев на 5 руках?";
+    questions[3] = "Укол делают каждые полчаса. Сколько нужно минут, чтобы сделать три укола?";
+    questions[4] = "Пять свечей горело, две потухли. Сколько свечей осталось?";
+    return questions;
+}
+int questionsCount = 5;
+string[] questions = GetQuestions(questionsCount);
 
-    static string[] GetQuestions(int questionsCount)
-    {
-        string[] questions = new string[questionsCount];
-        questions[0] = "Сколько будет два плюс два умноженное на два?";
-        questions[1] = "Бревно нужно распилить на 10 частей. Сколько распилов нужно сделать?";
-        questions[2] = "На двух руках 10 пальцев. Сколько пальцев на 5 руках?";
-        questions[3] = "Укол делают каждые полчаса. Сколько нужно минут, чтобы сделать три укола?";
-        questions[4] = "Пять свечей горело, две потухли. Сколько свечей осталось?";
-        return questions;
-    }
-    int questionsCount = 5;
-    string[] questions = GetQuestions(questionsCount); 
-
-    static int[] GetAnswers(int questionsCount)
-    {
-        int[] answers = new int[questionsCount];
-        answers[0] = 6;
-        answers[1] = 9;
-        answers[2] = 25;
-        answers[3] = 60;
-        answers[4] = 2;
-        return answers;
-    }
-string lastAnswer;
-string[] yesVariants = { "yes", "y", "true", "да", "еще бы", "только об этом и мечтаю" };
+static int[] GetAnswers(int questionsCount)
+{
+    int[] answers = new int[questionsCount];
+    answers[0] = 6;
+    answers[1] = 9;
+    answers[2] = 25;
+    answers[3] = 60;
+    answers[4] = 2;
+    return answers;
+}
 do
 {
     bool lastAction;
@@ -62,27 +59,27 @@ do
         }
     }
     Console.WriteLine("количество правильных ответов пользователя " + nameUser + " = " + GetDiagnosis(countRightAnswers));
-    Console.WriteLine("Повторить тест? (Yes/No)"); 
-    lastAnswer = Console.ReadLine();
-} while (yesVariants.Contains(lastAnswer, StringComparer.OrdinalIgnoreCase));
-    
-       
-    
-    static int GetDiagnosis(int x)
+} while (RepeatQuestions() == true);
+static int GetDiagnosis(int x)
+{
+    switch (x)
     {
-        switch (x)
-        {
-            case 0: Console.WriteLine("Ваш диагноз - идиот"); break;
-            case 1: Console.WriteLine("Ваш диагноз - кретин"); break;
-            case 2: Console.WriteLine("Ваш диагноз - дурак"); break;
-            case 3: Console.WriteLine("Ваш диагноз - нормальный"); break;
-            case 4: Console.WriteLine("Ваш диагноз - талант"); break;
-            case 5: Console.WriteLine("Ваш диагноз - гений!!!"); break;
-        }
-        return x;
+        case 0: Console.WriteLine("Ваш диагноз - идиот"); break;
+        case 1: Console.WriteLine("Ваш диагноз - кретин"); break;
+        case 2: Console.WriteLine("Ваш диагноз - дурак"); break;
+        case 3: Console.WriteLine("Ваш диагноз - нормальный"); break;
+        case 4: Console.WriteLine("Ваш диагноз - талант"); break;
+        case 5: Console.WriteLine("Ваш диагноз - гений!!!"); break;
     }
-
-
-
-
+    return x;
+}
+static bool RepeatQuestions()
+{
+    Console.WriteLine("Хотите повторить? [Да/нет]");
+    string answer = Console.ReadLine();
+    string[] yesVariants = { "yes", "y", "true", "да", "еще бы", "только об этом и мечтаю" };
+    if (yesVariants.Contains(answer, StringComparer.OrdinalIgnoreCase))
+    { return true; }
+    else { return false; }
+}
 
